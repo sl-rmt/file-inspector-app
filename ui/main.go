5382ad67708ff0fa80315f2b4ad0a4605fd9f764
Scott Lester <scott@redmaple.tech>
2024-05-08 11:52:27 +0100

    added file picker

--- ui/main.go
+++ ui/main.go
@@ -8,6 +8,7 @@ import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -20,17 +21,42 @@ func main() {
 	myApp := app.New()
 	window := myApp.NewWindow(appName)
 
-	// add button
-	// TODO use NewButtonWithIcon
-	button := widget.NewButton("Choose file", func() {
-		log.Println("Button pressed")
-	})
+	// add text for the middle in a vertical scroller
+	middle := container.NewVScroll(canvas.NewText("Select a file...", color.White))
 
-	middle := canvas.NewText("Select a file...", color.White)
+	// add buttons in horizontal box
+	buttons := container.NewHBox()
+
+	// TODO use NewButtonWithIcon
+	//var targetFile string
+
+	buttons.Add(widget.NewButton("Select File", func() {
+		log.Println("Select file was clicked!")
+
+		onChosen := func(f fyne.URIReadCloser, err error) {
+			if err != nil {
+				log.Printf("Error from file picker: %s\n", err.Error())
+				return
+			}
+			if f == nil {
+				log.Println("Nil result from file picker")
+				return
+			}
+
+			log.Printf("chosen: %v", f.URI())
+			//targetFile = f.URI().String()
+		}
+
+		dialog.ShowFileOpen(onChosen, window)
+	}))
+
+	// TODO use NewButtonWithIcon
+	buttons.Add(widget.NewButton("Reset", func() {
+		log.Println("Reset was clicked!")
+	}))
 
 	// set layout to borders
-	//top := canvas.NewText("Select a file...", color.White)
-	content := container.NewBorder(nil, button, nil, nil, middle)
+	content := container.NewBorder(nil, buttons, nil, nil, middle)
 
 	// set default size
 	window.Resize(fyne.NewSize(600, 900))
